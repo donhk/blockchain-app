40a56e8b8b30de2091ea34b75d64f93dc25952bd
Frederick Alvarez <fralvarez@linkedin.com>
2023-05-05 18:24:09 -0500

    new changes

--- blockchain-info-app/src/main.rs
+++ blockchain-info-app/src/main.rs
@@ -6,14 +6,27 @@ mod blockchain_address;
 mod blockchain_status;
 mod blockchain_transactions;
 
-use crate::blockchain_status::BlockchainStatus;
-use crate::blockchain_address::BlockchainAddress;
-use crate::blockchain_transactions::BlockchainTransaction;
+use {
+    crate::blockchain_status::BlockchainStatus,
+    crate::blockchain_address::BlockchainAddress,
+    crate::blockchain_transactions::BlockchainTransaction,
+    dotenv,
+    std::{io, thread, time},
+};
 
-fn main() {
-    let blockchain_status = blockchain_info::blockchain_status_request();
+fn blockchain_info_app(address: &str) {
+    let blockchain_status: BlockchainStatus = blockchain_info::blockchain_status_request();
     println!("Querying {} - chain {}",
              &blockchain_status.blockbook.coin,
              &blockchain_status.backend.chain
-    )
+    );
+    let blockchain_address: BlockchainAddress = blockchain_info::blockchain_address_request(address);
+    println!("Analyzing transactions for Bitcoin address {}", &blockchain_address.address);
+    let sleep_time = time::Duration::from_millis(2500);
+    thread::sleep(sleep_time);
+}
+
+fn main() {
+    let entered_address = dotenv::var("WALLET").expect("Failed to read WALLET");
+    blockchain_info_app(&entered_address);
 }
